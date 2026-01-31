48bb149e1a476bbd06d5bf3fbc2b86e725a7fe15
Swizel De Melo <swizeldemelo@gmail.com>
2026-01-31 13:26:00 +0000

    Formatted the contact page and added validation to login page

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Validators, FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[
+      Validators.required, Validators.minLength(8),
+      Validators.maxLength(32)]),
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+  
+  loginSuccess=false;
+
+  onSubmit(){
+    if(this.loginForm.valid){
+    console.log('Form submitted', this.loginForm.value);
+    this.loginSuccess=true;
+    //resets the form
+    this.loginForm.reset();
+  }
+}
 
 }
