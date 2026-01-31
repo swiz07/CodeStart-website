eb0e2da78a54695f43891c8b164d3fa9bd635e48
Swizel De Melo <swizeldemelo@gmail.com>
2026-01-31 01:28:22 +0000

    Add validations and submit message to Contact page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer,CommonModule,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- frontend/src/app/components/contact-us/contact-us.ts
+++ frontend/src/app/components/contact-us/contact-us.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { Validators, FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule, FormsModule],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.scss',
 })
 export class ContactUs {
+  contactForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
+
+  submissionMessage:string=''; //this has the success message once the user clicks submit btn
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message(){
+    return this.contactForm.get('message');
+  }
+  
+  onSubmit(){
+    if(this.contactForm.valid){
+    console.log('Form submitted', this.contactForm.value);
+
+    //show success message
+    this.submissionMessage='Thank you! Your message has been sent successfully.';
+
+    //resets the form
+    this.contactForm.reset();
+  }
+}
 
 }
